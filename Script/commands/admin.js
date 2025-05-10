22919b685c170670ebbb00578d173b924bb80f43
Ariyan-Naima3812 <117514366+Ariyan-Naima3812@users.noreply.github.com>
2025-05-10 23:13:40 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -23,41 +23,36 @@ const time = process.uptime(),
 		seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
-var link = ["https://i.postimg.cc/MKtkjNt6/inbound2956052169393787307.jpg", 
+var link = ["https://i.postimg.cc/1tJHSkn2/1740504548780.jpg", 
             
-            "https://i.postimg.cc/SxpdLfQg/inbound1909913016344335212.png", 
+            "https://i.postimg.cc/prSJhKBB/1740146513802.jpg", 
             
-            "https://i.postimg.cc/MKtkjNt6/inbound2956052169393787307.jpg",
+            "https://i.postimg.cc/7Z0nhGJg/1728637121854.jpg",
 
-            "https://i.postimg.cc/MKtkjNt6/inbound2956052169393787307.jpg",
+            "https://i.postimg.cc/02JpXMfL/IMG-20240704-225140.jpg",
        
-              "https://i.postimg.cc/MKtkjNt6/inbound2956052169393787307.jpg",
-
-                    "https://i.postimg.cc/MKtkjNt6/inbound2956052169393787307.jpg",
-
-"https://i.postimg.cc/MKtkjNt6/inbound2956052169393787307.jpg",
             
             ""];
   
-var callback = () => api.sendMessage({body:`𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮𝐥𝐚𝐢𝐤𝐮𝐦🌺💚𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 
+var callback = () => api.sendMessage({body:`𝗔𝘀𝘀𝗮𝗹𝗮𝗺𝘂  𝗔𝗹𝗮𝗶𝗸𝘂𝗺🌺💚𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 
 ________________________________________
 
 ❇️𝐁𝐎𝐓 𝐍𝐀𝐌𝐄 : ${global.config.BOTNAME}
 
-❇️𝐁𝐎𝐓 𝐀𝐃𝐌𝐈𝐍 :  『🅼🅰🅷🅸🅼 🆇🅾🆆🅳🅷🆄🆁🆈』 
+❇️𝐁𝐎𝐓 𝐀𝐃𝐌𝐈𝐍 :  𝗔𝗥𝗜𝗬𝗔𝗡 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬 
 
 ❇️𝐀𝐃𝐃𝐑𝐄𝐒 : 🄿🅄🅃🄷🄸🅈🄰•🅁🄰🄹🅂🄷🄰🄷🄸
 
 _____________𝐂𝐎𝐍𝐓𝐀𝐂𝐓_____________
 
-❇️𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 : https://www.facebook.com/profile.php?id=100000227139245
+❇️𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 : https://www.facebook.com/Abal.Tor.Abbu.Irfan.Ariyan
 
 ❇️𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 2 :
-https://www.facebook.com/profile.php?id=100000227139245
+https://www.facebook.com/Ariyan.Chowdhury.official.3812
 
 ❇️𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗 : ${global.config.PREFIX}
 
-❇️𝐁𝐎𝐓 𝐎𝐖𝐍𝐄𝐑 : 𝐃𝐉-𝐌𝐀𝐇𝐈𝐌-}⛱️✨
+❇️𝐁𝐎𝐓 𝐎𝐖𝐍𝐄𝐑 : 𝗔𝗥𝗜𝗬𝗔𝗡 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬 
 
 𝐎𝐓𝐇𝐄𝐑 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍____________________
 
